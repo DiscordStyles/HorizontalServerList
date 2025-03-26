5470dd64f752a1ceb259105202e3235180911fc1
Gibbu <Gibbu_@hotmail.com>
2025-03-27 10:33:19 +1100

    Upgrades people, upgrades.

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -2,19 +2,12 @@
 export default {
 	meta: {
 		name: 'Horizontal Server List',
-		author: 'Gibbu#1211',
-		version: '2.1.0',
+		author: 'Gibbu',
+		version: '3.0.0',
 		description: 'Moves the server list from the left to the top of Discord.',
 		source: 'https://github.com/DiscordStyles/HorizontalServerList',
 		invite: 'ZHthyCw',
-		website: 'https://gibbu.me',
 	},
-	baseImport: 'https://discordstyles.github.io/HorizontalServerList/dist/HorizontalServerList.css',
-	base: {
-		output: 'dist/dist',
-	},
-	addons: [
-		['src/addons/_bottomhsl.scss', 'dist/bottomhsl.css'],
-		['src/addons/_resizablelist.scss', 'dist/resizablelist.css'],
-	],
+	github: 'DiscordStyles',
+	addons: [{ target: 'src/addons/_bottomhsl.scss' }],
 };
